be53456dca7ed023a0aefdb766fade0b60f36334
ssahn0806 <ssahn0806@naver.com>
2022-11-22 20:39:26 +0900

    ssu:fixed again

--- FrontVue/src/store/modules/SearchStore.js
+++ FrontVue/src/store/modules/SearchStore.js
@@ -25,24 +25,14 @@ const SearchStore = {
   actions: {
     [Constant.GET_BLOGS](context) {
       return restApi.get("/api/searchs/blogs").then(({ data }) => {
-<<<<<<< HEAD
-=======
-        console.log(data);
->>>>>>> f72cd9b629cd12f2e78fb3956e9614a4350086b7
         context.commit(Constant.SET_BLOGS, data);
       });
     },
     [Constant.GET_NEWS](context, payload) {
       if (!payload) {
         return restApi.get("/api/searchs/news").then(({ data }) => {
-<<<<<<< HEAD
         console.log(data.result);
         context.commit(Constant.SET_NEWS, data.result);
-=======
-          console.log(data.result);
-
-          context.commit(Constant.SET_NEWS, data.result);
->>>>>>> f72cd9b629cd12f2e78fb3956e9614a4350086b7
         });
       }
       if (payload) {
